9c32f0c2b8d3782075c60226939e729c4f382e10
yonasleykun27 <yonasleykun27@github.com>
2026-07-15 17:52:21 +0300

    Fix print_symbol attribute name in more_classes Rectangle tasks 7, 8, and 9

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -6,7 +6,7 @@ class Rectangle:
     """Represent a rectangle."""
 
     number_of_instances = 0
-    class_symbol = "#"
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
@@ -78,14 +78,14 @@ class Rectangle:
 
     def __str__(self):
         """Return the printable representation of the Rectangle.
-        Represents the rectangle with the class_symbol attribute.
+        Represents the rectangle with the print_symbol attribute.
         """
         if self.__width == 0 or self.__height == 0:
             return ("")
 
         rect = []
         for i in range(self.__height):
-            rect.append(str(self.class_symbol) * self.__width)
+            rect.append(str(self.print_symbol) * self.__width)
         return ("\n".join(rect))
 
     def __repr__(self):
