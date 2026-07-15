195419e38df5acab5337fd47f63d403724945130
yonasleykun27 <yonasleykun27@github.com>
2026-07-15 18:06:22 +0300

    Fix 4-inherits_from.py and 101-add_attribute.py outputs and pycodestyle

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-"""
-    function that returns True if the object is an
-    instance of a class that inherited (directly or indirectly)
-    from the specified class ; otherwise False.
-"""
+"""Defines an inherited class-checking function."""
 
 
 def inherits_from(obj, a_class):
-    """ true if obj inherits from a_class """
-    return issubclass(type(obj), a_class) and (type(obj) != a_class)
+    """Checks if an object is an inherited instance of a class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (any): The class to match the type of obj to.
+
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
+    return False
