0f08a9a157a6c07379947c553e1856f61d7de489
yonasleykun27 <yonasleykun27@gmail.com>
2026-08-16 17:49:58 +0300

    Fix headers, URLs, endpoints, and implementations in python-network_0 and python-network_1

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
-"""
+"""Sends a POST request with an email parameter using requests."""
 import sys
-import urllib.request
+import requests
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    value = {"email": sys.argv[2]}
+    r = requests.post(url, data=value)
+    print(r.text)
