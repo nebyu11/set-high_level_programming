b5e8da6b380aef6d524e94eacfa18c8876a7509b
yonasleykun27 <yonasleykun27@gmail.com>
2026-08-16 18:19:54 +0300

    Support both alx-intranet and intranet mock host endpoints in 0-hbtn_status and 4-hbtn_status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status using urllib."""
+"""Fetches status from intranet using urllib."""
 import urllib.request
 
 
 if __name__ == "__main__":
-    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    try:
+        req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+        with urllib.request.urlopen(req) as response:
+            body = response.read()
+    except Exception:
+        req = urllib.request.Request("https://intranet.hbtn.io/status")
+        with urllib.request.urlopen(req) as response:
+            body = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status using the requests package."""
+"""Fetches status using the requests package."""
 import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://alx-intranet.hbtn.io/status")
+    try:
+        r = requests.get("https://alx-intranet.hbtn.io/status")
+    except Exception:
+        r = requests.get("https://intranet.hbtn.io/status")
+
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
